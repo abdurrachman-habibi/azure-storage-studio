d7e140bfd6b7d344b8a673a38cef3dd7fd6df0f9
Habibi <guilvee@gmail.com>
2015-06-09 00:22:19 +0800

    Init Layout

--- src/app/layout/shell.js
+++ src/app/layout/shell.js
@@ -6,5 +6,28 @@
 
     function ShellController($location, $mdMedia, $mdSidenav, $rootScope, $timeout, $window) {
         var vm = this;
+
+        $rootScope.$on('$routeChangeSuccess', function () {
+            var lastScrollTop = 0;
+            if (vm.isLoading) {
+                vm.isLoading = false;
+
+                document.addEventListener('optimizedScroll', function (event) {
+
+                    $timeout(function () {
+                        var st = event.detail.scrollTop;
+                        if (st !== undefined) {
+                            if (st - lastScrollTop < -2) {
+                                vm.isScrollingDown = false;
+                            }
+                            else if (st - lastScrollTop > 2) {
+                                vm.isScrollingDown = true;
+                            }
+                            lastScrollTop = st;
+                        }
+                    });
+                });
+            }
+        });
     }
 })();
